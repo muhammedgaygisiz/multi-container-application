956d92c58fc59d1c091ee1a3fb32f6499df26800
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2019-11-24 11:51:37 +0100

    Stabilize and some changes

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return this.state.seenIndexes &&
+        this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
